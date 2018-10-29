c0174d97fe09b9956868ed60fbf5b31ba50ca6b5
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2018-10-29 15:05:53 +0000

    generic external service builder for node files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ove-external-services
+# Open Visualisation Environment
+
+Open Visualisation Environment (OVE) is an open-source software stack, designed to be used in large high resolution display (LHRD) environments like the [Imperial College](http://www.imperial.ac.uk) [Data Science Institute's](http://www.imperial.ac.uk/data-science/) [Data Observatory](http://www.imperial.ac.uk/data-science/data-observatory/).
+
+We welcome collaboration under our [Code of Conduct](https://github.com/ove/ove/blob/master/CODE_OF_CONDUCT.md).
+
+## Build Instructions
+
+The build system is based on Bash, Docker and docker-compose. This works well with node-based services.
+
+### Build an external service
+
+Clone the desired repository e.g. Tuoris https://github.com/oserban/tuoris.git then run:
+
+```sh
+./build.sh -s tuoris -p 7080 --chrome
+```
+The -s option represents the service name (which has to match the folder name) and -p represents the port number the service exposes.
+
+**Note:** Tuoris requires a valid chrome install, so the --chrome flag needs to be passed.
+
+This tool generates a Dockerfile and a docker-compose.yml file for each service. These files can be found in the **generated** folder.
---
+++ build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+scriptPath=$(dirname "$(readlink -f "$0")")
+cd ${scriptPath}/
+
+TEMPLATES_PATH="templates"
+
+export GENERATED_PATH="generated"
+mkdir -p ${GENERATED_PATH}
+
+function deactivate_env {
+  echo "Deactivating env variables ..."
+  unset SERVICE_VERSION
+  unset SERVICE_NAME
+  unset SERVICE_PORT
+  unset GENERATED_PATH
+}
+
+function display_help() {
+  echo "Build image for external service"
+  echo
+  echo "usage: build.sh [option]..."
+  echo "   -s, --service     The name of the service to build, same as the folder name"
+  echo "   -p, --port        The port(s) exposed by the service"
+  echo "   --chrome          Include chrome in the build"
+  echo "   -v, --version     The version of the service"
+}
+
+version=`git describe --tags --exact-match 2> /dev/null`
+if [ $? -ne 0 ]; then
+  version="latest"
+fi
+
+BASE_IMAGE="Dockerfile"
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -h|--help)
+      display_help
+      exit 0
+      ;;
+    -s|--service)
+      service="$2"
+      shift
+      ;;
+    -p|--port)
+      port="$2"
+      shift
+      ;;
+    --chrome)
+      BASE_IMAGE="Dockerfile.chrome"
+      ;;
+    -v|--version)
+      version="$2"
+      echo "Recognized user version = ${version}"
+      shift
+      ;;
+    *)
+      echo "Unrecognised option: $key"
+      echo
+      display_help
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+export SERVICE_VERSION=${version}
+export SERVICE_NAME=${service}
+export SERVICE_PORT=${port}
+
+trap deactivate_env EXIT SIGINT SIGTERM
+
+echo "Using version = ${SERVICE_VERSION}"
+
+if [[ ! -d "${SERVICE_NAME}" ]]; then
+  echo "Invalid service = ${SERVICE_NAME} provided"
+  exit 1
+fi
+
+if [[ -z "${SERVICE_PORT}" ]]; then
+  echo "Invalid port provided"
+  exit 1
+fi
+
+echo "Building service = ${SERVICE_NAME} exposing port = ${SERVICE_PORT} using baseImage = ${BASE_IMAGE}"
+
+envsubst < ${TEMPLATES_PATH}/docker-compose.yml > ${GENERATED_PATH}/docker-compose.${SERVICE_NAME}.yml
+envsubst < ${TEMPLATES_PATH}/${BASE_IMAGE} > ${GENERATED_PATH}/Dockerfile.${SERVICE_NAME}
+envsubst < ${TEMPLATES_PATH}/pm2.json > ${SERVICE_NAME}/pm2.json
+
+docker-compose -f ${GENERATED_PATH}/docker-compose.${SERVICE_NAME}.yml build
+if [ $? -ne 0 ]; then
+  # fail if the image build process failed
+  exit 1
+fi
+
